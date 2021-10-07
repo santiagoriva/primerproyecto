c4a5b7395a61d3a6f4319d60f44178377ea75246
SANTIAGORIVA <>
2021-10-07 17:28:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// Esta intuccion muestra un mensaje en la consola
+console.log("hola consola");
+
+// variables
+var miNombre = "Santiago";
+var edad = 40;
+
+// costantes 
+const miApellido = "Crocker"
+
+console.log("mi nombre es" +  miNombre); 
+miNombre = "Pedro";
+console.log("ya no, mi nombre ahora es " + miNombre);
+
+//miApellido = "Rivapalacio" esto no es posible 
+
+// operaciones basicas
+/* las operaciones basicas 
+-suma (+)
+-resta (-)
+-multiplicacion (*)
+-divicion (/)
+estan ya definidas en javaScrip */
+
+// encontrar el año de nacimientro daba la edad 
+const anio = 2021 
+var anioDeNac = anio - edad; 
+console.log("si tienes " + edad + " años, entonces naciste en el año " + anioDeNac);
